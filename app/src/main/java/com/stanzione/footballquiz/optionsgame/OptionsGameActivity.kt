d77fefb7f979c9fcd1fcc45a2d7620cbc9e20af9
Leandro Stanzione <leandro.stanzione@olxbr.com>
2023-12-27 15:32:38 -0300

    Implementing OptionsGameActivity template

--- app/src/main/java/com/stanzione/footballquiz/optionsgame/OptionsGameActivity.kt
+++ app/src/main/java/com/stanzione/footballquiz/optionsgame/OptionsGameActivity.kt
@@ -3,7 +3,15 @@ package com.stanzione.footballquiz.optionsgame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.stanzione.footballquiz.ui.theme.FootballQuizTheme
 
 class OptionsGameActivity : ComponentActivity() {
@@ -13,7 +21,31 @@ class OptionsGameActivity : ComponentActivity() {
         setContent {
 
             FootballQuizTheme {
-                Text(text = "Options Game Activity")
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Red)
+                ) {
+                    Text(text = "Options Game Activity")
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.Blue)
+                            .weight(5f)
+                    ) {
+                        Text(text = "Image block")
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.Green)
+                            .weight(3f)
+                    ) {
+                        Text(text = "Options block")
+                    }
+                }
+
             }
 
         }
