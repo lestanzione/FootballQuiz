c3d6c61532b5d086788248d1bbab76ebbc6b9d1b
Leandro Stanzione <leandro.stanzione@olxbr.com>
2024-11-28 15:56:26 -0300

    Creating sample levels

--- app/src/main/java/com/stanzione/footballquiz/levels/data/repository/datasource/local/LevelLocalDataSourceImpl.kt
+++ app/src/main/java/com/stanzione/footballquiz/levels/data/repository/datasource/local/LevelLocalDataSourceImpl.kt
@@ -11,6 +11,39 @@ class LevelLocalDataSourceImpl : LevelLocalDataSource {
     }
 
     private fun getAllLevels(): List<Level> {
-        return emptyList()
+        return listOf(
+            Level(
+                id = 1,
+                label = "Level 1",
+                enabled = true,
+                questions = 10,
+                score = 0,
+                categoryId = 1
+            ),
+            Level(
+                id = 2,
+                label = "Level 2",
+                enabled = false,
+                questions = 10,
+                score = 0,
+                categoryId = 1
+            ),
+            Level(
+                id = 3,
+                label = "Level 3",
+                enabled = false,
+                questions = 10,
+                score = 0,
+                categoryId = 1
+            ),
+            Level(
+                id = 1000,
+                label = "Level 1",
+                enabled = true,
+                questions = 10,
+                score = 0,
+                categoryId = 2
+            ),
+        )
     }
 }
